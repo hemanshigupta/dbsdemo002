5f688f88ee78367d2d9663fe1e355afa1d0f164a
hemanshigupta <hemanshigupta17195@gmail.com>
2017-10-10 12:39:39 +0100

    update index.php with html tag

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php
-echo "Hello world from Hemanshi \n";
-echo "This is my new app";
+echo "<h1> Hello world </h1>";
+echo "<p> Hello world  from heroku</p>";
+echo "<p> This is a new paragraph </p>";
