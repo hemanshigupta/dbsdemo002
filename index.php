3b39fb7eca820fd446434215e999edc4091a9616
hemanshigupta <hemanshigupta17195@gmail.com>
2017-10-10 13:34:34 +0100

    Update index.php

--- index.php
+++ index.php
@@ -2,3 +2,4 @@
 echo "<h1> Hello world verson 0.2 </h1>";
 echo "<p> Hello world  from heroku</p>";
 echo "<p> This is a new paragraph </p>";
+echo "<p><a href= 'peopleapi.php'> This is link to peopleapi</a></p>";
